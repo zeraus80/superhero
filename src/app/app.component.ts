d00e628a997fc58be4fe6308ce506fa78f20db19
Javier Navarro <jnavarro@leftfieldlabs.com>
2018-12-04 21:43:20 -0500

    Building angular app from scratch

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello World!</h1>'
+})
+export class AppComponent {
+    
+    constructor() {
+        console.log('I am Angular!');
+    }
+}
